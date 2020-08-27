50d09946c386a3309b2ea24ba6057d2ac9591a17
Sandy Nicko Mac Corzeta <sandynicko@windowslive.com>
2020-08-27 22:42:46 +0700

    Change env into Heroku

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://simple-crud4interview.local/';
+$config['base_url'] = 'https://simple-crud4website.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
